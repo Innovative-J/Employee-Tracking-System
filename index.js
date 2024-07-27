088fd00ff59b02b8ee9f5c73b5a292153e73830b
Innovative-J <juan.cia05678@gmail.com>
2024-07-26 19:18:16 -0500

    added functions on index.js

--- index.js
+++ index.js
@@ -56,18 +56,12 @@ const viewAllDepartments = () => {
   db.query(`SELECT name FROM department`, (err, result) => {
     if (err) throw err;
     console.log("Viewing All Departments:");
-    // console.log(result.rows);
     result.rows.forEach((department) => console.log(department.name));
     employee_tracker();
   });
-};
-// copy this for the rest of the options 
+}; 
 
-
-
-
-
-//   same for this one as well for the rest.
+//   view all roles
   const viewAllRoles = () => {
     db.query(`SELECT * FROM role`, (err, result) => {
       if (err) throw err;
@@ -76,6 +70,61 @@ const viewAllDepartments = () => {
       result.fields.forEach((role) => console.log(role.name));
     });
   };
+
+  // view all employees
+  const viewAllEmployees = () => {
+    db.query(`SELECT name FROM employees`, (err, result) => {
+      if (err) throw err;
+      console.log("Viewing employee");
+      result.rows.forEach((employee)=> console.log(employee.name))
+    });
+  };
+
+  // just for reference
+  // case "Add A Department":
+  //   addDepartment();
+  //   break;
+  // case "Add A Role":
+  //   addRole();
+  //   break;
+  // case "Add An Employee":
+  //   addEmployee();
+  //   break;
+  // case "Update An Employee Role":
+  //   updateEmployeeRole();
+  //   break;
+  // case "Log Out":
+  //   db.end();
+  //   break;
+          // "Add A Department",
+          // "Add A Role",
+          // "Add An Employee",
+          // "Update An Employee Role",
+          // "Log Out",
+
+
+
+  const addDepartment = () => {
+    inquirer
+    .prompt ([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the department:'
+      }
+    ]).then(answer => {
+      db.query(`INSERT INTO department (name) VALUES ()`, [answer.name], (err,result) => {
+        if (err) throw err;
+        console.log("Department added!");
+        employee_tracker();
+      });
+    });
+  };
+
+
+  // //  .then((answers) => {
+  //   switch (answers.choice) {
+  //     case "View All Departments":
 
 // Start server after DB connection
 db.connect((err) => {
